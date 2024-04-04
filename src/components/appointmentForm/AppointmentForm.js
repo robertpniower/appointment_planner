f1c7d970947ce79a506315578f825f2370c9b3a6
robertpniowerdh <165269031+robertpniowerdh@users.noreply.github.com>
2024-04-04 12:54:41 +0200

    update AppointmentForm

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from'../contactPicker/ContactPicker';
 
 const getTodayString = () => {
   const [month, day, year] = new Date()
@@ -24,9 +25,41 @@ export const AppointmentForm = ({
     <form onSubmit={handleSubmit}>
     <label>
       <input
-      
+        type="text"
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+        placeholder="Appointment title"
       />
     </label>
+    <br/>
+    <ContactPicker contacts={contacts} />
+    <br/>
+    <label>
+      <input
+        type="date"
+        name="date"
+        value={date}
+        min={getTodayString()}
+        onChange={(e) => setDate(e.target.value)}
+        required
+        placeholder="Appointment date"
+      />
+    </label>
+    <br/>
+    <label>
+      <input
+        type="time"
+        name="time"
+        value={title}
+        onChange={(e) => setTime(e.target.value)}
+        required
+        placeholder="Appointment time"
+      />
+    </label>
+    <br/>
+    <input type="submit" value='Add Appointment' />
 
     </form>
   );
